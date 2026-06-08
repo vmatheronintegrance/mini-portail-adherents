58a09897bd3b1bffca119d081fe09423b20076d5
Victor M. <victor.matheron.ext@integrance.fr>
2026-06-08 13:29:20 +0200

    Bases formulaires

--- src/app/accueil/accueil.ts
+++ src/app/accueil/accueil.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-accueil',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './accueil.html',
   styleUrl: './accueil.scss',
 })
 export class Accueil {
+  champInput: string = "";
 
+  
+  valider() {
+    if(this.champInput.length > 5) {
+      console.log("Valider");
+    } else {
+      console.log("Erreur");
+    }
+  }
 }
--- src/app/profil/profil.ts
+++ src/app/profil/profil.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profil',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profil.html',
   styleUrl: './profil.scss',
 })
 export class Profil {
+  
+  email = new FormControl<string>('', [Validators.required]);
+  
+  submit($event: Event) {
+    $event.preventDefault();
+    console.log(this.email.value);
+  }
+
 
 }
